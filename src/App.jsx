274f3710de5723118df7663286220b8ee71905a1
Xpanetel <ibrohim@MAC.local>
2024-03-11 07:37:35 +0500

    new

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Ap from "./components/Ap";
+
+function App() {
+  return (
+    <div className="app">
+      <Ap />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Ap.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function Ap() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.example.com/data")
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Ap;
